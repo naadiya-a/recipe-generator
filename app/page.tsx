a031e24c44551e7083de3d68dbb9295d644ecac2
Naadiya Ahmedabadi <49887062+naadiya-a@users.noreply.github.com>
2024-12-24 19:21:02 +0000

    Switched to clerk login

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { cookies } from "next/headers";
 import ChatWindow from "./chat-window";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  if (!cookies().has('sessionToken')) {
-    redirect('/login');
-  }
   return (
     <div>
-      <ChatWindow/>
+      <ChatWindow />
     </div>
   );
 }
