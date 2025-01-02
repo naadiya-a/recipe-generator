1bbc635a2b860b8ffe19b20918963082786730bc
Naadiya Ahmedabadi <49887062+naadiya-a@users.noreply.github.com>
2025-01-02 23:08:57 +0000

    Minor changes

--- data/auth.ts
+++ data/auth.ts
@@ -3,8 +3,5 @@ import { auth } from '@clerk/nextjs/server'
 export default async function isSignedIn() {
   const { userId } = await auth()
 
-  if (!userId) {
-    return false;
-  };
-  return true;
+  return !!userId;
 }
