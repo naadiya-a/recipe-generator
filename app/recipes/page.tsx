1b5c2e0a881163545a7ffcc745eb5c8625745ef4
Naadiya Ahmedabadi <49887062+naadiya-a@users.noreply.github.com>
2025-01-10 04:36:29 +0000

    Added a message to show when no recipes are saved

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -5,6 +5,19 @@ import Link from "next/link";
 export default async function RecipeCards() {
   const recipes = await getRecipes();
 
+  if (recipes.length == 0) {
+    return (
+      <div className="container mx-auto max-w-2xl min-h-screen flex flex-col">
+        <h1 className="text-3xl font-bold text-center mt-8">Recipes</h1>
+        <div className="flex-grow flex items-center justify-center -mt-20">
+          <div className="text-center text-lg">
+            No recipes saved. Generate one now! 👩‍🍳
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container mx-auto py-8 max-w-2xl">
       <h1 className="text-3xl font-bold mb-6 text-center">Recipes</h1>
